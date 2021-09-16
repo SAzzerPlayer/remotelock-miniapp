5d4724890207ae8cd8cdc7028a88008c3ebcb2ce
Lohvyn Danyil <logvin24820@gmail.com>
2021-09-16 21:35:42 +0300

    Lock portrait orientation on start

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -3,6 +3,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./store";
 import { TopTabNavigator } from "./navigation/TopTabNavigator";
+import * as ScreenOrientation from "expo-screen-orientation";
+
+(function () {
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
+    .then(() => console.log("[APP]: Portrait orientation is locked."))
+    .catch((e) =>
+      console.error("[APP]: Failed to lock portrait orientation!\n", e)
+    );
+})();
 
 export const App: React.FC = () => (
   <ReduxProvider store={store}>
